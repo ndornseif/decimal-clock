24b2b7bc0faa8e00ba1240b5eac7d6a98d700479
ndornseif <5382403+ndornseif@users.noreply.github.com>
2023-10-24 19:10:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# decimal-clock
+
+A clock that displays the current time in a decimal format.  
+
+## Overview
+This code implements a decimal clock that gets its time from an NTP server.  
+It is written for the LDM8E display controller and uses the LDM8E-Library.  
+See the GitHub repos for the [hardware](https://github.com/ndornseif/LDM8E-LEDDisplayController) and [library](https://github.com/ndornseif/LDM8E-Library) for more info.  
+
+![Decimal clock showing 7882 (18:55:00.480)](Images/clock_running.png)  
+
+## Installation
+The code should work on any LDM8E as long as the dimming mode and WiFi credentials are properly configured.  
+
+## Time format  
+The clock represents the time of day with a number between 0 and 9999, where each increment represents 8.64 seconds (one ten-thousandth of a day).  
+See this table for some example times:    
+| Decimal Time |   24h Time   |
+|--------------|--------------|
+|     0000     | 00:00:00.000 |
+|     0001     | 00:00:08.640 |
+|     1000     | 02:24:00.000 |
+|     5000     | 12:00:00.000 |
+|     9000     | 21:36:00.000 |
+|     9999     | 23:59:51.260 |
+
+## Dependencies
+
+- [Arduino ESP32 WiFi](https://github.com/espressif/arduino-esp32/tree/master)  
+- [LDM8E-Library](https://github.com/ndornseif/LDM8E-Library)  
+
+## Other
+Published under GPL-3.0 license.  
---
+++ decimal-clock/decimal-clock.ino
@@ -0,0 +1,131 @@
+/*
+  DecimalClock.ino
+  LDM8E decimal clock for LDM2 module display.
+  This program displays the current time on a LDM8E as decimal time.
+  Created 2023-10-22 by N.Dornseif
+  Writen for the LDM8E hardware only.
+  More info on the LDM8E-Library:
+  https://github.com/ndornseif/LDM8E-Library
+  More info on the LDM8E hardware:
+  https://github.com/ndornseif/LDM8E-LEDDisplayController
+  Published under GPL-3.0 license.
+*/
+#include <LDM8EDriver.h>
+#include <WiFi.h>
+#include "time.h"
+#include "sntp.h"
+
+/*
+  This parameter defines the dimming mode
+  0=Chip Enable PWM (LDM2)
+  1=Split ground PWM (LDM1)
+  2=Analog dimming (Both)
+  See github for more info on dimming:
+  https://github.com/ndornseif/LDM8E-LEDDisplayController
+*/
+const byte dimmingMode = 2;
+
+//Defines the number of diplay modules connected
+//Having fewer than four reduces your display precision
+const byte numModules = 4;
+
+//Enter Wifi credentials here
+const char* ssid       = "";
+const char* password   = "";
+
+//Consider replacing with ntp servers in your region
+const char* ntpServer1 = "ptbtime1.ptb.de";
+const char* ntpServer2 = "ptbtime4.ptb.de";
+
+//Configure your timezone here
+const long  gmtOffset_sec = 3600;
+const int   daylightOffset_sec = 3600;
+const char* time_zone = "CET-1CEST,M3.5.0,M10.5.0/3";
+
+//Configure the brightness during day and night
+//255 = max, 0 = min
+const byte dayBrightness = 240;
+const byte nightBrightness = 42;
+
+//If the activity LED should blink when the time is updated
+const bool blinkActyLED = true;
+
+/*
+Configure the time when night brightness is active
+Set value as decimal time:
+One increment corresponds to 8.64 seconds.
+0000=00:00:00 ; 0001=00:00:08 ; 9999=23:59:51 ; 5000 = 12:00:00 
+*/
+const int nightStart = 8750; //21:00
+const int nightEnds = 2083; //05:00
+
+//Set up a display named "mydisplay"
+LDM8EDisplay mydisplay(dimmingMode, numModules);
+
+void checkBrightness(int currentTime){
+  if(currentTime > nightStart || currentTime < nightEnds){
+    mydisplay.setBrightness(nightBrightness);
+  }else{
+    mydisplay.setBrightness(dayBrightness);
+  }
+}
+
+int timeToDecimal(struct tm *t){
+  word siminutes;
+  word sihours;
+  word siseconds;
+  float totalsiseconds;
+  char timeHour[3];
+  char timeMinute[3];
+  char timeSecond[3];
+  strftime(timeHour,3, "%H", t);
+  strftime(timeMinute,3, "%M", t);
+  strftime(timeSecond,3, "%S", t);
+  siminutes = ((timeMinute[0]-'0')*10)+(timeMinute[1]-'0');
+  sihours = ((timeHour[0]-'0')*10)+(timeHour[1]-'0');
+  siseconds = ((timeSecond[0]-'0')*10)+(timeSecond[1]-'0');
+  totalsiseconds = (siminutes * 60) + (sihours * 3600) + siseconds;
+  return (int)((totalsiseconds/86400.0) * 10000.0);
+}
+
+void setup() {
+  //Initalize the display
+  mydisplay.begin();
+  //Turn off all segments
+  mydisplay.setDisplay(0);
+  //Set brightness to 50%
+  mydisplay.setBrightness(128);
+
+  //Turn on all "G" segments
+  //Displays a line: "----"
+  mydisplay.setDisplay(0x02020202);
+  
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2);
+  Serial.printf("Connecting to WiFi Network: %s ", ssid);
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED){
+      delay(1000);
+      Serial.print("-");
+  }
+  Serial.println("WiFi Connected");
+}
+
+void sendTimeToDisplay(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("No time available (yet)");
+  }else{
+    int currentTime = timeToDecimal(&timeinfo);
+    checkBrightness(currentTime);
+    mydisplay.writeDisplay(currentTime);
+    Serial.println(currentTime);
+  }
+}
+
+void loop() {
+  delay(2000);
+  mydisplay.setActyLED(blinkActyLED);
+  sendTimeToDisplay();
+  delay(50);
+  mydisplay.setActyLED(0);
+}
